14c1208e765f8f75e9d9e866b7a45c849508054e
Daniel <dplaton@gmail.com>
2016-05-18 23:44:09 +0300

    Add more solutions

--- longest_palindrome.py
+++ longest_palindrome.py
@@ -1,18 +1,18 @@
+
 def longest_palindromic(text):
-    if len(text) == 1:
+    if len(text) == 1 or text == text[::-1]:
         return text
 
-    longest = ""
+    longest = []
     for idx in range(0,len(text)-1):
         string = text[idx:]
-        for i, e in enumerate(string,1):
-            if string[:i] == string[i-1::-1]:
-                longest = string[:i] if len(string[:i]) > len(longest) else longest
+        longest.extend([string[:i] for i in range(1, len(string)) if string[:i] == string[i-1::-1]])
 
-    return longest
+    return max(longest, key=len)
 
 if __name__ == '__main__':
     assert longest_palindromic("artrartrt") == "rtrartr", "The Longest"
+    assert longest_palindromic("racecar") == "racecar", "The First"
     assert longest_palindromic("abacada") == "aba", "The First"
     assert longest_palindromic("aaaa") == "aaaa", "The A"
     assert longest_palindromic("abc") == "a", "Nothing"
--- the_good_radix.py
+++ the_good_radix.py
@@ -1,13 +1,17 @@
 """
 https://checkio.org/mission/good-radix/
 """
+import string
+all_chars = string.digits + string.ascii_uppercase
 
 def checkio(number):
     """
     Your solution
     """
-    return 0
+    min_radix = all_chars.index(max(number).upper())
 
+    return min(filter(lambda k:int(number, k) % (k-1) == 0, range(min_radix+1, len(all_chars) + 1)) or [0])
+    
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
     assert checkio(u"18") == 10, "Simple decimal"
@@ -15,4 +19,5 @@ if __name__ == '__main__':
     assert checkio(u"222") == 3, "3rd test"
     assert checkio(u"A23B") == 14, "It's not a hex"
     assert checkio(u"IDDQD") == 0, "k is not exist"
+    assert checkio(u"ZZZ") == 36, "Last radix"
     print('Local tests done')
