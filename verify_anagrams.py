17b6bb828ee053cccba035b81187aa9e8131327c
Daniel Platon <dplaton@gmail.com>
2016-12-10 22:31:34 +0200

    Delete moved files

--- verify_anagrams.py
+++
@@ -1,9 +0,0 @@
-verify_anagrams = lambda x, y: sorted(x.lower().replace(" ","")) == sorted(y.lower().replace(" ", ""))
-
-if __name__ == '__main__':
-    #These "asserts" using only for self-checking and not necessary for auto-testing
-    assert isinstance(verify_anagrams("a", 'z'), bool), "Boolean!"
-    assert verify_anagrams("Programming", "Gram Ring Mop") == True, "Gram of code"
-    assert verify_anagrams("Hello", "Ole Oh") == False, "Hello! Ole Oh!"
-    assert verify_anagrams("Kyoto", "Tokyo") == True, "The global warming crisis of 3002"
-
